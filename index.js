dad97083cb29845270d1e283b653ce065cf3a73c
ibrahimbtaz <ibrahimbtaz@gmail.com>
2023-10-31 16:29:31 +0700

    add button show bomb

--- index.js
+++ index.js
@@ -45,6 +45,35 @@
 
 	
 
+	 ab.click(function () {
+	 	if (c.includes(i)) {
+	 		if (
+	 			$(this).hasClass('grid-item') ||
+	 			$(this).hasClass('bomb-marked')
+	 		) {
+	 			hp--;
+	 			$('#hp').text(hp);
+	 			$(this)
+	 				.removeClass('grid-item bomb-marked')
+	 				.addClass('bomb')
+	 				.delay(10)
+	 				.queue(function (next) {
+	 					// alert('Boom! Kamu menemukan bom!');
+	 					if (hp === 0) {
+	 						setTimeout(() => {
+	 							alert('Game Over');
+	 							createGrid(); // Memulai permainan baru
+	 							hp = 3; // Reset nyawa
+	 							$('#hp').text(hp); // Mengupdate tampilan nyawa
+	 						}, 10);
+	 					}
+	 				}); //jika kotak memiliki bom maka akan diberi warna merah setelah di klik dan akan menampilkan alert
+	 		}
+	 	} else {
+	 		$(this).addClass('when-clicked'); // Menambahkan class "when-clicked" untuk penanda ketika kotak di klik
+	 	}
+	 });
+
 
 
 	// for(let i = 1; i <= 5; i++) {
@@ -198,3 +227,212 @@
 	// }
 
 	// console.log(...c);
+
+	// (function ($) {
+	// 	// Fungsi untuk menangani klik pada kotak dengan bom
+	// 	$.fn.handleBomb = function () {
+	// 		const a = this;
+	// 		a.on('click', '.grid-item', function () {
+	// 			const clickedCell = $(this);
+	// 			const cellIndex = clickedCell.index();
+
+	// 			if (c.includes(cellIndex)) {
+	// 				handleBombCellClick(clickedCell);
+	// 			} else {
+	// 				handleEmptyCellClick(clickedCell);
+	// 			}
+	// 		});
+
+	// 		function handleBombCellClick(cell) {
+	// 			if (cell.hasClass('grid-item') || cell.hasClass('bomb-marked')) {
+	// 				hp--;
+	// 				$('#hp').text(hp);
+	// 				cell
+	// 					.removeClass('grid-item bomb-marked')
+	// 					.addClass('bomb')
+	// 					.delay(10)
+	// 					.queue(function (next) {
+	// 						if (hp === 0) {
+	// 							setTimeout(() => {
+	// 								alert('Game Over');
+	// 								createGrid();
+	// 								hp = 3;
+	// 								$('#hp').text(hp);
+	// 							}, 10);
+	// 						}
+	// 					});
+	// 			}
+	// 		}
+
+	// 		function handleEmptyCellClick(cell) {
+	// 			cell.addClass('when-clicked');
+	// 		}
+	// 	};
+	// })(jQuery);
+
+	// // Menggunakan fungsi $.fn.handleBomb() pada elemen dengan ID "grid"
+	// $('#grid').handleBomb();
+
+
+	function handleGridItemClick() {
+		const ab = $(this);
+		const i = ab.data('row');
+		const j = ab.data('col');
+		const c = ab.data('bombLocations');
+		let hp = ab.data('hp');
+
+		if (c.includes(i * 8 + j)) {
+			if (ab.hasClass('bomb-orange')) {
+				hp--;
+				ab
+					.removeClass('bomb-orange')
+					.addClass('bomb')
+					.delay(10)
+					.queue(function (next) {
+						alert('Boom! Kamu menemukan bom!');
+						next();
+					});
+				if (hp == 0) {
+					alert('Game Over');
+					new_board();
+					hp = 5;
+				}
+			}
+			$('#hp').text(hp);
+		} else {
+			ab.css('background-color', 'white');
+		}
+	}
+
+	function createGrid(a, c, hp) {
+		// Create the grid
+		for (let i = 0; i < 8; i++) {
+			for (let j = 0; j < 8; j++) {
+				const ab = $('<div></div>').addClass('grid-item').data('row', i).data('col', j).data('bombLocations', c).data('hp', hp);
+
+				if (c.includes(i * 8 + j)) {
+					ab.addClass('bomb-orange');
+				}
+
+				a.append(ab);
+			}
+		}
+	}
+
+	function new_board() {
+		const a = $('#grid'); // Menentukan grid yang akan menampung board
+		let b = 8; // Jumlah bom
+		let c = []; // Menyimpan lokasi bom secara acak
+		let hp = 5;
+
+		// Reset the grid
+		a.empty();
+
+		// Reset the bomb locations
+		c = [];
+
+		// Generate new bomb locations
+		while (c.length < b) {
+			const r = Math.floor(Math.random() * 64); // 8x8 grid
+			if (!c.includes(r)) {
+				c.push(r);
+			}
+		}
+
+		hp = 5;
+		$('#hp').text(hp);
+
+		createGrid(a, c, hp);
+	}
+
+	// Create the grid on page load
+	new_board();
+
+	// Add click event listener to reset button
+	$('button').click(function () {
+		new_board();
+	});
+
+
+	    < script type = "text/javascript" >
+	    	$(document).ready(function () {
+	    		const a = $('#grid');
+	    		const j = 8 * 8; // Jumlah row dan col
+	    		let b = 10; // Jumlah bom
+	    		let c = []; // Menyimpan lokasi bom secara acak
+
+	    		let hp = 3;
+	    		$('#hp').text(hp);
+
+	    		function createGrid(a) {
+	    			for (let i = 0; i < j; i++) {
+	    				const ab = $('<div></div>').addClass('grid-item');
+	    				// if (c.includes(i)) {
+	    				//   // ab.addClass('bomb-marked') //jika kotak memiliki bom makan akan diberi mark
+	    				//   // console.log(i)
+	    				// }
+	    				a.append(ab);
+	    			}
+	    		}
+
+	    		function new_board() {
+	    			a.empty();
+	    			c = [];
+	    			while (c.length < b) {
+	    				const r = Math.floor(Math.random() * 64); // 8x8 grid
+	    				if (!c.includes(r)) {
+	    					c.push(r);
+	    				}
+	    			}
+	    			console.log(...c);
+	    			createGrid(a);
+	    		}
+
+	    		function handle_click() {
+	    			const i = $(this).index();
+	    			console.log(i);
+	    			if (c.includes(i)) {
+	    				if (
+	    					$(this).hasClass('grid-item') ||
+	    					$(this).hasClass('bomb-marked')
+	    				) {
+	    					hp--;
+	    					$('#hp').text(hp);
+	    					$(this)
+	    						.removeClass('grid-item bomb-marked')
+	    						.addClass('bomb')
+	    						.delay(10)
+	    						.queue(function (next) {
+	    							// if (hp === 0) {
+	    							//   setTimeout(() => {
+	    							//     alert('Game Over');
+	    							//     new_board(); // Memulai permainan baru
+	    							//     hp = 3; // Reset nyawa
+	    							//     $('#hp').text(hp); // Mengupdate tampilan nyawa
+	    							//   }, 10);
+	    							// }
+	    							// alert('Boom! Kamu menemukan bom!');
+	    						}); //jika kotak memiliki bom maka akan diberi warna merah setelah di klik dan akan menampilkan alert
+	    				}
+	    			} else {
+	    				$(this).addClass('when-clicked'); // Menambahkan class "when-clicked" untuk penanda ketika kotak di klik
+	    			}
+	    		}
+
+	    		// function show_bomb() {
+	    		//   $('.grid-item').each(function (i) {
+	    		//     if (c.includes(i)) {
+	    		//       console.log(i);
+	    		//       $(this).addClass('bomb-marked');
+	    		//     }
+	    		//   });
+	    		//   console.log(c);
+	    		// }
+
+	    		new_board();
+
+	    		$('#grid').on('click', '.grid-item', handle_click);
+	    		$('.new_board').click(new_board);
+	    		$('.show_bomb').click(show_bomb);
+	    	}); <
+	    /script>
